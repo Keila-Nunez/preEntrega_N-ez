0d8c916795c1a0ef43058bb6910186eaf51a9a49
Keila Nuñez <aliekeila1991@gmail.com>
2022-11-09 19:55:52 -0300

    se agrega arrays, objects, métodos, variables

--- introduccion.js
+++ introduccion.js
@@ -1,61 +1,127 @@
+const servicios = [
+    { nombre: "cejas", precio: 2000},
+    { nombre: "pestañas", precio: 2000},
+    { nombre: "depilación de espalda", precio: 90000},
+    { nombre: "depilación de bozo", precio: 17000},
+    { nombre: "depilación de axilas", precio: 20000},
+    { nombre: "depilación de cavado", precio: 34000},
+    { nombre: "electroporación", precio: 3200},
+    { nombre: "limpieza de cutis", precio: 4000},
+    { nombre: "radiofrecuencia facial", precio: 4000},
+];
 
 //Iniciar Sesión 
 let usuario = prompt("TicTac Beauty te da la bienvenida para iniciar sesión debes ingresar tu usuario");
-let contrasenia = prompt("Ingresa tu contraseña");
+let contrasenia = prompt("Ingresa tu contraseña"); 
+if (usuario == ""){
+    alert("El campo usuario no puede estar vacío");
+}   else if (contrasenia == "") {
+        alert ("El campo contraseña no puede estar vacío");
+}   else {
+        alert ("Bienvenid@ " + usuario + ", " + "ingreso válido");
+}
 
-    if (usuario == ""){
-            alert("El campo usuario no puede estar vacío");
-        } else if (contrasenia == "") {
-            alert ("El campo contraseña no puede estar vacío");
-        } else {
-            alert ("Bienvenid@ " + usuario + ", " + "ingreso válido");
-    }  
-  
-//Menú
+
+//Menú        
 function captura () {
-    let opcion = parseInt (prompt("Seleccione una opción:\n 1- Sacar turno\n 2- Ver calendario\n 3- Salir "));
-    while (opcion != 3 ) {
-        switch (opcion) {
-            case 1:
-                let sacarTurno = parseInt (prompt("Qué servicio desea:\n Depilación:\n 1- Bozo\n 2- Espalda\n 3-Pecho\n  Cejas y pestañas:\n 4-Microshading\n 5- Lifting de pestañas\n 6- Retoque\n Manicuría:\n 7- Mantenimiento\n 8- Esmaltado permanente\n 9- Esmaltado semipermanente"));
-                if (sacarTurno == 1) {
-                    alert("Turno reservado para depilación de bozo, el monto a pagar es de $1.000");
-                } else if (sacarTurno == 2){
-                    alert ("Turno reservado para depilación de espalda, el monto a pagar es de $21.000"); 
-                } else if (sacarTurno == 3){
-                    alert ("Turno reservado para depilación de pecho, el monto a pagar es de $25.000");
-                } else if (sacarTurno == 4){
-                    alert ("Turno reservado para Cejas y pestañas: Microshading, el monto a pagar es de $36.000");
-                } else if (sacarTurno == 5){
-                    alert ("Turno reservado para Cejas y pestañas: Lifting de pestañas, el monto a pagar es de $3.000");
-                } else if (sacarTurno == 6){
-                    alert ("Turno reservado para Cejas y pestañas: Retoque, el monto a pagar es de $7.000");
-                } else if (sacarTurno == 7){
-                    alert ("Turno reservado para Manicuría: Mantenimiento, el monto a pagar es de $2.000");
-                } else if (sacarTurno == 8){
-                    alert ("Turno reservado para Manicuría: Esmaltado permanente, el monto a pagar es de $2.000");
-                } else if (sacarTurno == 9){
-                    alert ("Turno reservado para Manicuría: Esmaltado semipermanente, el monto a pagar es de $1.500");
-                } else if (isNaN (sacarTurno)){
-                    alert("Debe ingresar una opción valida, recuerde usar números");
-                } else if (sacarTurno >= 10 ){
-                    alert ("La opción ingresada no es válida");
-                    return sacarTurno;
-                } else if (sacarTurno == ""){
-                    alert("Debe ingresar una opción");
-                } else{
-                    alert ("Para reservar un turno debe escoger una opción"); 
-                }
+    let carrito = [];
+    let seleccion = prompt("Seleccione una opción:\n 1- Sacar turno\n 2- Salir ");
+        
+    while (seleccion != "1" && seleccion != "2") { 
+        alert("Por favor, ingresa 1 o 2")
+        seleccion = prompt("Seleccione una opción:\n 1- Sacar turno\n 2- Salir");
+    }
+
+    if (seleccion == "1"){ 
+        alert("A continuación nuestra lista de servicios")
+        let todosLosServicios = servicios.map((servicios)=> servicios.nombre + " " + servicios.precio + "$" );
+        alert(todosLosServicios.join("\n"))
+    }   else if (seleccion == "2") {
+        alert("Muchas gracias, vuelva pronto");
+        let usuario = prompt("TicTac Beauty te da la bienvenida para iniciar sesión debes ingresar tu usuario");
+        let contrasenia = prompt("Ingresa tu contraseña"); 
+
+            if (usuario == ""){
+                alert("El campo usuario no puede estar vacío");
+            } else if (contrasenia == "") {
+                alert ("El campo contraseña no puede estar vacío");
+            } else {
+                alert ("Bienvenid@ " + usuario + ", " + "ingreso válido");
+            } 
+        seleccion = prompt("Seleccione una opción:\n 1- Sacar turno\n 2- Salir");
+    }
+
+
+    while (seleccion != "2") {
+        let servicios = prompt("Agrega un servicio a tu carrito");
+        let precio = 0;
+    
+        if (servicios == "cejas" || servicios == "pestañas" || servicios == "depilación de espalda" || 
+            servicios ==  "depilación de bozo" || servicios == "depilación de axilas" || servicios == "depilación de cavado" || 
+            servicios ==  "electroporación" || servicios == "limpieza de cutis" || servicios == "radiofrecuencia facial"){
+            switch(servicios){
+                case "cejas": 
+                    precio = 2000;
                 break;
-            case 2: 
-                    alert ("Tienes un turno reservado para: Depilación de espalda\n Tienes un turno reservado para: Lifting de pestañas\n Tienes un turno reservado para: Esmaltado semipermanente"); 
+                case "pestañas": 
+                    precio = 2000;
                 break;
-            case 3: 
-                    alert ("Vuelva pronto");
+                case "depilación de espalda": 
+                    precio = 90000;
+                break;
+                case "depilación de bozo": 
+                    precio = 17000;
+                break;
+                case "depilación de axilas": 
+                    precio = 20000;
+                break;
+                case "depilación de cavado": 
+                    precio = 34000;
+                break;
+                case "electroporación": 
+                    precio = 3200;
+                break;
+                case "limpieza de cutis": 
+                    precio = 4000;
+                break; 
+                case "radiofrecuencia facial": 
+                    precio = 4000;
                 break;
                 default:
-                    alert ("Opción incorrecta") ;  
+                break;
+            }
+            
+            let fecha = prompt("Qué día de la semana desea asistir a su turno? Debe tener en cuenta que los domingos no prestamos servicios"); 
+            if (fecha == "lunes" || fecha == "martes" || fecha == "miercoles" || fecha == "jueves" || fecha == "viernes" || fecha == "sabado"){
+                carrito.push({servicios, precio, fecha});  
+                console.log(carrito); 
+                       
+            }   else {
+                    alert("Debe ingresar un día de la semana, recuerde que los domingos no prestamos servicios:\n lunes\n martes\n miercoles\n jueves\n viernes\n sabado\n ");
+                    fecha = prompt("Qué día de la semana desea asistir a su turno? Debe tener en cuenta que los domingos no prestamos servicios");    
+            }   
+
+        }   else {
+                alert("No tenemos ese servicio");
         }
-        opcion = parseInt (prompt("Seleccione una opción:\n 1- Sacar turno\n 2- Ver calendario\n 3- Salir "));
+  
+        seleccion = prompt("Desea seleccionar otro servicio?");
+        if(seleccion === "no"){
+            alert("Gracias por su reserva");
+            carrito.forEach((carritoFinal) => {
+                console.log (`Servicio: ${carritoFinal.servicios}\n Fecha: ${carritoFinal.fecha}\n Total a pagar por servicio: ${carritoFinal.precio}`)
+            })
+            break;    
+        }    
     }
+ 
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    console.log ("El total a pagar por todos los servicios es de: " + "$" + total);
 }
+     
+
+    
+           
+    
+        
+
